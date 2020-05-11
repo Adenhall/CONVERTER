acb904ded2192ff2a3e0fa789aea0e7d186e0ebe
Adenhall <jamesadenhall@gmail.com>
2020-05-11 17:29:44 +0700

    Added EUR

--- java.js
+++ java.js
@@ -9,21 +9,33 @@ let exchangeRatio = 23228.63
 let currency1 = prompt("From what currency would you like to exchange?")
 let currency2 = prompt("To what currency would you like to exchange?")
 
-
 function fromVNDtoUSD () {
   let result = from / exchangeRatio
   console.log("The equivalent amount is ",new Intl.NumberFormat('usd',{ style: 'currency', currency: 'USD' }).format(result.toFixed(2)))
 }
 function fromUSDtoVND () {
   let result = from * exchangeRatio
-  console.log("The equivalent amount is ",new Intl.NumberFormat('usd',{ style: 'currency', currency: 'VND'}).format(result.toFixed(2)))
+  console.log("The equivalent amount is ",new Intl.NumberFormat('vnd',{ style: 'currency', currency: 'VND'}).format(result.toFixed(2)))
 }
-
+function fromVNDtoEUR () {
+  let result = from / exchangeRatio
+  console.log("The equivalent amount is ",new Intl.NumberFormat('eur',{ style: 'currency', currency: 'EUR' }).format(result.toFixed(2)))
+}
+function fromEURtoVND () {
+  let result = from * exchangeRatio
+  console.log("The equivalent amount is ",new Intl.NumberFormat('vnd',{ style: 'currency', currency: 'VND'}).format(result.toFixed(2)))
+}
 
 if (currency1 === "vnd" && currency2 === "usd") {
   fromVNDtoUSD()
 }
 else if (currency1 === "usd" && currency2 === "vnd") {
   fromUSDtoVND()
+}
+else if (currency1 === "eur" && currency2 === "vnd") {
+  fromEURtoVND()
+}
+else if (currency1 === "vnd" && currency2 === "eur") {
+  fromVNDtoEUR()
 }
 else console.log("Currency not available")
