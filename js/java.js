106c6b8823359e6a5af6b1ce3ffa470ebbb638a7
Adenhall <jamesadenhall@gmail.com>
2020-05-13 11:57:05 +0700

    Added objects Converter working

--- js/java.js
+++ js/java.js
@@ -1,71 +1,50 @@
-let amountInput = document.getElementById('amountInput')
-let result = amountInput.value
-console.log(result)
+let amount = document.getElementById('amount')
 let convertButton = document.getElementById('convertButton');
 
-
-let exchangeRatioUSD = 23228.63
-let exchangeRatioEUR = 25061.24
-let exchangeRatioEURUSD = 1.0743
 
 
 let fromCurrency = document.getElementById("fromCurrency");
 let toCurrency = document.getElementById("toCurrency");
-console.log(fromCurrency.value)
-console.log(toCurrency.value)
 
-function fromVNDtoUSD () {
-  let result=amountInput.value;
-  let convertedAmount = result / exchangeRatioUSD
-  console.log("The equivalent amount is ",new Intl.NumberFormat('usd',{ style: 'currency', currency: 'USD' }).format(convertedAmount.toFixed(2)))
-  document.getElementById("results").innerHTML = `your money in USD is ${new Intl.NumberFormat('usd',{ style: 'currency', currency: 'USD' }).format(convertedAmount.toFixed(2))}`;
+
+const currencyRatio = {
+  vnd: {
+    usd: 0.000043,
+    krw: 0.051,
+    eur: 0.000039,
+    vnd: 1
+  },
+  usd: {
+    usd: 1,
+    krw: 1193.27,
+    eur: 0.9,
+    vnd: 23235.5
+  },
+  eur: {
+    usd: 0.00084,
+    krw: 1,
+    eur: 0.00075,
+    vnd: 19.47
+  }
 }
-function fromUSDtoVND () {
-    let result = amountInput.value;
-    let convertedAmount = result * exchangeRatioUSD
-    console.log("The equivalent amount is ", new Intl.NumberFormat('vnd', { style: 'currency', currency: 'VND' }).format(convertedAmount.toFixed(2)))
-    document.getElementById("results").innerHTML = `your money in VND is ${new Intl.NumberFormat('vnd',{ style: 'currency', currency: 'VND' }).format(convertedAmount.toFixed(2))}`;
+console.log(currencyRatio.vnd)
+
+function formatCurrency(type, value) {
+  const formatter = new Intl.NumberFormat(type, {
+    currency: type,
+    style: "currency"
+  });
+  return formatter.format(value);
 }
-function fromVNDtoEUR () {
-   let result = amountInput.value;
-   let convertedAmount = result / exchangeRatioEUR
-   console.log("The equivalent amount is ", new Intl.NumberFormat('eur', { style: 'currency', currency: 'EUR' }).format(convertedAmount.toFixed(2)))
-   document.getElementById("results").innerHTML = `your money in EUR is ${new Intl.NumberFormat('eur',{ style: 'currency', currency: 'EUR' }).format(convertedAmount.toFixed(2))}`;
+
+
+function convertMoney() {
+  let from = fromCurrency.value
+  let to = toCurrency.value
+  let ratio = currencyRatio[from][to]
+  let result = (amount.value * ratio).toFixed(2);
+  document.getElementById("results").innerHTML = `Your equivalent amount is ${result}`;
+  console.log(amount)
+  console.log(ratio)
 }
-function fromEURtoVND () {
-   let result = amountInput.value;
-   let convertedAmount = result * exchangeRatioEUR
-   console.log("The equivalent amount is ", new Intl.NumberFormat('vnd', { style: 'currency', currency: 'VND' }).format(convertedAmount.toFixed(2)))
-   document.getElementById("results").innerHTML = `your money in VND is ${new Intl.NumberFormat('vnd',{ style: 'currency', currency: 'VND' }).format(convertedAmount.toFixed(2))}`;
-}
-function fromEURtoUSD () {
-   let result = amountInput.value;
-   let convertedAmount = result * exchangeRatioEURUSD
-   console.log("The equivalent amount is ", new Intl.NumberFormat('usd', { style: 'currency', currency: 'USD' }).format(convertedAmount.toFixed(2)))
-   document.getElementById("results").innerHTML = `your money in USD is ${new Intl.NumberFormat('usd',{ style: 'currency', currency: 'USD' }).format(convertedAmount.toFixed(2))}`;
-}
-function fromUSDtoEUR () {
-   let result = amountInput.value;
-   let convertedAmount = result / exchangeRatioEURUSD
-   console.log("The equivalent amount is ", new Intl.NumberFormat('eur', { style: 'currency', currency: 'EUR' }).format(convertedAmount.toFixed(2)))
-   document.getElementById("results").innerHTML = `your money in EUR is ${new Intl.NumberFormat('eur',{ style: 'currency', currency: 'EUR' }).format(convertedAmount.toFixed(2))}`;
-}
-if (fromCurrency === "VND" && toCurrency === "USD") {
-  convertButton.addEventListener('click',fromVNDtoUSD)
-}
-else if (fromCurrency === "USD" && toCurrency === "VND") {
-  convertButton.addEventListener('click',fromUSDtoVND)
-}
-else if (fromCurrency === "EUR" && toCurrency === "VND") {
-  convertButton.addEventListener('click',fromEURtoVND)
-}
-else if (fromCurrency === "VND" && toCurrency === "EUR") {
-  convertButton.addEventListener('click',fromVNDtoEUR)
-}
-else if (fromCurrency === "USD" && toCurrency === "EUR") {
-  convertButton.addEventListener('click',fromUSDtoEUR)
-}
-else if (fromCurrency === "EUR" && toCurrency === "USD") {
-  convertButton.addEventListener('click',fromEURtoUSD)
-}
-else console.log("Currency not available")
+convertButton.addEventListener('click',convertMoney)
